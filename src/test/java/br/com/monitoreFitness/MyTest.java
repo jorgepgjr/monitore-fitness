74062390a765eb80716b4a138f970058fc90e3ff
Jorge <jorgepgjr@gmail.com>
2014-09-08 18:59:51 -0300

    first commit

---
+++ src/test/java/br/com/monitoreFitness/MyTest.java
@@ -0,0 +1,16 @@
+package br.com.monitoreFitness;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class MyTest {
+
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+}
